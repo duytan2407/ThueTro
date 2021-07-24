d1ce3ea2752800364ae08126fcaa027492bf8cf3
Luanne <viluan922@gmail.com>
2021-07-24 19:09:00 +0700

    no message

---
+++ ThueTro/Controllers/timThueeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThueTro.Controllers
+{
+    public class timThueeController : Controller
+    {
+        // GET: timThuee
+        public ActionResult TimThue()
+        {
+
+            return View();
+        }
+    }
+}
